984555691b7105b34bab3721b8c57abb25e7bcd8
badihaki <jojackblack@gmail.com>
2022-01-22 12:34:44 -0500

    Initial Commit - Mech Battler
    Initial commit of Mech Battler.
    - create dev room scene to test mechanics, gfx, colliders, etc
    - Started working on systems for characters
    - added base player object script to control PC
    - Added player character scriptable object for easy creation of characters

---
+++ mechabattler/Assets/_Player/_Scripts/PlayerCharacter.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="NewCharacter",menuName ="Player/Player Character")]
+public class PlayerCharacter : ScriptableObject
+{
+    public GameObject playerCharObj;
+    public int health;
+}
---
+++ mechabattler/Assets/_Player/_Scripts/PlayerObject.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObject : MonoBehaviour
+{
+    public PlayerCharacter playerCharacter;
+    public Transform trackedChar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
